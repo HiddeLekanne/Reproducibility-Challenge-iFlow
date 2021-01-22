f4c2001b2f69039b99d41dace8e2273c8abfad8c
hiddelekanne <hiddelekanne@gmail.com>
2021-01-22 18:15:19 +0100

    Refactor in process, partly operational though.

--- iFlow/plot_correlation.py
+++ iFlow/plot_correlation.py
@@ -11,39 +11,13 @@ from lib.models import iVAE
 
 from lib.utils2 import model_and_data_from_log
 
-def model_estimations(x, u, model):
-    if isinstance(model, iVAE):
-        _, z_est = model.elbo(x, u)
-    else:
-        import operator
-        from functools import reduce
-        total_num_examples = x.shape[0]
-        model.set_mask(total_num_examples)
-        z_est, _ = model.inference(x, u)
-
-    z_est = z_est.detach().numpy()
-    return z_est
-
-def permutate_corresponding_dimensions(s, z):
-    d = z.shape[1]
-    cc = np.corrcoef(s, z, rowvar=False)[:d, d:]
-    pairs = linear_sum_assignment(-1 * abs(cc))
-    z[:, pairs[0]] = z[:, pairs[1]]
-    return z
 
 def plot_latent_correlation(dset, model, n_samples, sample_offset):
     # Obtain source (s) and approximation (z_est)
     x = dset.x
     u = dset.u
     s = dset.s.detach().numpy()
-    if isinstance(model, iVAE):
-        _, z_est = model.elbo(x, u)
-    else:
-        total_num_examples = x.shape[0]
-        model.set_mask(total_num_examples)
-        z_est, _ = model.inference(x, u)
-
-    z_est = z_est.detach().numpy()
+    z_est = model_predict(x, u, model)
 
     # Find corresponding dimensions between s and z_dim
     d = x.shape[1]
@@ -78,14 +52,6 @@ def plot_latent_correlation(dset, model, n_samples, sample_offset):
         fig.suptitle('iFlow')
     plt.show()
 
-def create_2D_performance_sub_plot(x, labels, ax, cmap, title="", mcc=None):
-    if mcc:
-        title += f' (MCC: {mcc:.2f})'
-    
-    ax.scatter(x[:, 0], x[:, 1], c=torch.argmax(labels, dim=1), cmap=cmap, alpha=0.9, s=3)
-    ax.set_title(label=title)
-    ax.set_xticks([])
-    ax.set_yticks([])
 
 def create_2D_performance_plot(dset, model_iVAE, model_iFlow):
     # Assert correct model type to assure same layout
@@ -98,8 +64,8 @@ def create_2D_performance_plot(dset, model_iVAE, model_iFlow):
     s = dset.s.detach().numpy()
 
     # Obtain approximations
-    z_est_iVAE = model_estimations(x, u, model_iVAE)
-    z_est_iFlow = model_estimations(x, u, model_iFlow)
+    z_est_iVAE = model_predict(x, u, model_iVAE)
+    z_est_iFlow = model_predict(x, u, model_iFlow)
 
     # Find corresponding dimensions between s with iVAE/iFlow estimations
     z_est_iVAE = permutate_corresponding_dimensions(s, z_est_iVAE)
--- iFlow/plot_mcc.py
+++ iFlow/plot_mcc.py
@@ -9,55 +9,61 @@ from lib.metrics import mean_corr_coef as mcc
 from lib.models import iVAE
 from lib.utils2 import model_and_data_from_log
 
-class Experiments:
+class Experiment_folder:
 
-    def __init__(self, main_dir):
-        self.experiment_paths = set([f.path for f in os.scandir(args["dirs"][i]) if f.is_dir()])
-        self.experiments = {}
+    def __init__(self, main_dir, device):
+        experiment_paths = set([f.path for f in os.scandir(main_dir) if f.is_dir()])
+        self.experiments, self.path2seed, self.seed2path = {}, {}, {}
+        self.device = device
         for path in experiment_paths:
-            with open(path + '/log.json') as f:
+            with open(path + '/log/log.json') as f:
                 metadata = json.load(f)['metadata']
                 self.experiments[path] = metadata
                 self.path2seed[path] = int(metadata["file"].split("_")[6])
                 self.seed2path[int(metadata["file"].split("_")[6])] = path
-         
-    def get_ranked_on(self, attribute):
-        if attribute == "final_performance":
-            get_final_performance()
+        self.get_final_performance()
 
-        return sorted(self.experiments, key= lambda experiment: experiment[attribute])
+         
+    def get_ranked_list(self, attribute):
+        if attribute == "final_performance":
+            return sorted(self.experiments.values(), key=lambda experiment: int(experiment[attribute]))
+        elif attribute == "seed":
+            return sorted(self.experiments.values(), key=lambda experiment: int(experiment["file"].split("_")[6]))
+        else:
+            raise NotImplementedError("the attribute: " + str(attribute) +  " has not been implemented.")
     
     def get_final_performance(self):
         for experiment in self.experiments.keys():
-            with open(experiment + '/log.json') as f:
+            with open(experiment + '/log/log.json') as f:
                 json_file = json.load(f)
 
-            # if not already computed
+            # if already computed
             if json_file["metadata"].get("final_performance"):
-                if self.experiments[experiment].get("final_performance"):
-                    continue
-                else:
+                if not self.experiments[experiment].get("final_performance"):
                     self.experiments[experiment]["final_performance"] = json_file["metadata"]["final_performance"]
+                continue
+
             try:
-                model, dset, _ = model_and_data_from_log(experiment, args["device"])
+                model, dset, _ = model_and_data_from_log(experiment, self.device)
             except json.decoder.JSONDecodeError:
                 print(experiment, "couldn't be loaded")
                 continue
 
-            model, dset, _ = model_and_data_from_log(experiment)
             model.eval()
             final_performance = calculate_mcc(dset, model)
 
-            with open(experiment + '/log.json') as f:
+            with open(experiment + '/log/log.json', "r+") as f:
                 json_file = json.load(f)
                 json_file["metadata"]["final_performance"] = final_performance
+                f.seek(0)
                 json.dump(json_file, f)
 
-def calculate_mcc(dset, model):
-    ""
-    x = dset.x
-    u = dset.u
-    s = dset.s.detach().numpy()
+    def __len___(self):
+        return len(self.experiments)
+
+
+
+def model_predict(x, u, model):
     if isinstance(model, iVAE):
         _, z_est = model.elbo(x, u)
     else:
@@ -68,65 +74,45 @@ def calculate_mcc(dset, model):
         z_est, _ = model.inference(x, u)
 
     z_est = z_est.detach().numpy()
+    return z_est
+
+
+def calculate_mcc(dset, model):
+    ""
+    x = dset.x
+    u = dset.u
+    s = dset.s.detach().numpy()
+    z_est = model_predict(x, u, model)
     return mcc(z_est, s)
 
-def plot_experiments(args):
-    if not (all(args["dirs"]) or all(args["load_files"])):
-        raise ValueError("you need to either specify all experiment dirs or all result files")
 
-    if any(args["dirs"]) and any(args["load_files"]):
-        raise ValueError("you need to either all experiment dirs or all saved results files, both are specified")
-
-    length = max(len(args["dirs"]), len(args["load_files"]))
-
-    scores = {}
-    for i in range(length):
-        if not all(args["load_files"]):
-            experiments = [f.path for f in os.scandir(args["dirs"][i]) if f.is_dir() ]
-            X, Y = [], []
-            for experiment in experiments:
-                try:
-                    model, dset, metadata = model_and_data_from_log(experiment, args["device"])
-                except json.decoder.JSONDecodeError:
-                    print(experiment, "couldn't be loaded")
-                    continue
-
-                model.eval()
-                Y.append(calculate_mcc(dset, model))
-                print(metadata["file"])
-                X.append(int(metadata["file"].split("_")[6]))   
-            if args.get("save_files"):
-                with open(args["save_files"][i], 'wb') as f:
-                        np.save(f, [X, Y])
-
-                scores[args["save_files"][i]]
-            Y = [y for _,y in sorted(zip(X,Y))]
-            X = sorted(X)
-            plt.plot(X, Y)
-        else:
-            with open(args["load_files"][i], 'rb') as f:
-                X, Y = np.load(f)
-        
-            scores[args["load_files"][i]] = [X, Y]
-            Y = [y for _,y in sorted(zip(X,Y))]
-            X = sorted(X)
-            plt.plot(X, Y)
+def plot_mcc(experiment_folders):
+    for experiment_folder in experiment_folders:
+        Y = []
+        for experiment in experiment_folder.get_ranked_list("seed"):
+            Y.append(experiment["final_performance"])
+        X = list(range(1, len(Y) + 1))
+        plt.plot(X, Y)    
     plt.show()
-    return scores
 
 
-if __name__ == '__main__':
+def align_dimensions(s, z):
+    d = z.shape[1]
+    cc = np.corrcoef(s, z, rowvar=False)[:d, d:]
+    pairs = linear_sum_assignment(-1 * abs(cc))
+    z[:, pairs[0]] = z[:, pairs[1]]
+    return z
 
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--dirs', metavar='dirs', type=str, default=[None],  nargs="*",
-                    help='all experiments directories')
-    group.add_argument('--load_files', default=[None], type=str, help="files from which to load the results instead of recalculating", nargs="*")
-    parser.add_argument('--device', metavar='device', default="cpu", type=str, help='device, either cpu or cuda')
-    parser.add_argument('--save_file', default=[], type=str, help="file where the results are saved")
+# if __name__ == '__main__':
 
-    args = parser.parse_args()
-    args = vars(args.Namespace())
+#     parser = argparse.ArgumentParser()
+#     group = parser.add_mutually_exclusive_group(required=True)
+#     group.add_argument('--dirs', metavar='dirs', type=str, default=[None],  nargs="*",
+#                     help='all experiments directories')
+#     parser.add_argument('--device', metavar='device', default="cpu", type=str, help='device, either cpu or cuda')
 
-    plot_experiments(args)
+#     args = parser.parse_args()
+#     args = vars(args.Namespace())
+
+#     plot_experiments(args)
 
